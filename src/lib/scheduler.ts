8a254c985e5871f375caf0a8b20d07698fcbac0b
A91y <ayush.agr254@gmail.com>
2024-11-17 19:03:22 +0530

    update log cleanup schedules to run every 6 hours for info logs and every 2 days for error logs

--- src/lib/scheduler.ts
+++ src/lib/scheduler.ts
@@ -1,13 +1,13 @@
 import cron from "node-cron";
 import { cleanupErrorLogs, cleanupInfoLogs } from "../utils/cleanupLogs";
 
-// Schedule cleanup for info logs every 12 hours
-cron.schedule("0 */12 * * *", () => {
+// Schedule cleanup for info logs every 6 hours
+cron.schedule("0 */6 * * *", () => {
   cleanupInfoLogs();
 });
 
-// Schedule cleanup for error logs every 3 days
-cron.schedule("0 0 */3 * *", () => {
+// Schedule cleanup for error logs every 2 days
+cron.schedule("0 0 */2 * *", () => {
   cleanupErrorLogs();
 });
 
