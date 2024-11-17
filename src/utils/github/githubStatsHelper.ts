9f79c1d62d7f5e7a3a934dd4bde4ce32aab0baaf
A91y <ayush.agr254@gmail.com>
2024-11-17 22:13:01 +0530

    add keep-alive connection header and increase timeout for GitHub API requests

--- src/utils/github/githubStatsHelper.ts
+++ src/utils/github/githubStatsHelper.ts
@@ -17,6 +17,7 @@ async function getRepos(page: number, authHeader: string) {
         headers: {
           Authorization: `${authHeader}`,
           Accept: "application/vnd.github.v3+json",
+          Connection: "keep-alive",
         },
         timeout: 100000, // 100 seconds
       });
@@ -45,7 +46,9 @@ async function getTotalCommits(username: string, authHeader: string) {
         "Content-Type": "application/json",
         Authorization: `${authHeader}`,
         Accept: "application/vnd.github.cloak-preview",
+        Connection: "keep-alive",
       },
+      timeout: 100000, // 100 seconds
     });
     return res.data.total_count;
   } catch (error) {
@@ -67,6 +70,7 @@ async function getGithubGraphql(login: string, authHeader: string) {
       headers: {
         Authorization: `${authHeader}`,
         Accept: "application/vnd.github.v3+json",
+        Connection: "keep-alive",
       },
       data: {
         query: GRAPHQL_STATS_QUERY,
@@ -77,6 +81,7 @@ async function getGithubGraphql(login: string, authHeader: string) {
           includeDiscussionsAnswers: true,
         },
       },
+      timeout: 100000, // 100 seconds
     });
     return res.data.data;
   } catch (error) {
